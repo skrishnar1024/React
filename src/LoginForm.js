36a12bffe44d2a261085d40fd91cb37525af7aaa
skrishnar1024 <krishnabavanireddy@gmail.com>
2023-03-20 18:47:58 -0500

    React

---
+++ src/LoginForm.js
@@ -0,0 +1,14 @@
+function LoginForm(){
+    return <form>
+        <div>
+            <label>Username:</label>
+            <input type="text"></input>
+        </div>
+        <div>
+            <label>Password :</label>
+            <input type="password"></input>
+        </div>
+    </form>
+}
+
+export default LoginForm
